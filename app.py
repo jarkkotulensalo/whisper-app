a27346d19c1e1c117c081d041df39366069d04c7
Jarkko Tulensalogit <jtulensalo@gmail.com>
2024-03-09 17:01:10 +0200

    removed record audio feature from app

--- app.py
+++ app.py
@@ -4,23 +4,6 @@ import sounddevice as sd
 import wave
 import subprocess
 
-# Streamlit UI components
-st.title('Whisper API Transcription Demo')
-wav_file = st.file_uploader('Upload Audio File', type=['wav', 'm4a'])
-
-
-# Function to record audio from the microphone
-def record_audio(duration, filename):
-    fs = 44100  # Sample rate
-    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
-    sd.wait()  # Wait until recording is finished
-    wave.write(filename, fs, recording)
-    
-
-def m4a_to_wav(input_file, output_file):
-    command = ['ffmpeg', '-i', input_file, output_file]
-    subprocess.run(command)
-
 
 # Function to call the Whisper API
 def transcribe_audio(wav_data):
@@ -35,12 +18,23 @@ def transcribe_audio(wav_data):
         return f'Error: {str(e)}'
 
 
-# Handling user input
-if wav_file is not None:
+# Streamlit UI components
+st.title('Locally hosted Whisper-3 Transcription Demo')
+
+# load file
+audio_file = st.file_uploader('Upload Audio File', type=['wav', 'm4a', 'mp3'])
+
+# play uploaded audio file
+if audio_file is not None:
+    if st.button("Play audio"):
+        st.audio(audio_file, format='audio/ogg/m4a/wav/mp3')
+
+    # transcribe audio
     if st.button('Transcribe'):
         # create a spinner while waiting for the API response
         with st.spinner('Transcribing...'):
-            wav_data = wav_file.read()
+            wav_data = audio_file.read()
             text = transcribe_audio(wav_data)
         st.write('Transcription:')
         st.write(text)
+
